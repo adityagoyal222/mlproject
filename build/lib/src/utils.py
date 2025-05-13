86090f6366e6d4be39c530c74240fbb088385741
adityagoyal222 <goyaladitya85@gmail.com>
2025-05-13 22:50:44 +0200

    modified git ignore

--- build/lib/src/utils.py
+++ build/lib/src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 import dill
 
 
@@ -20,14 +21,21 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, np.ravel(y_train))
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, np.ravel(y_train))
+             
+            # model.fit(X_train, np.ravel(y_train))
 
             y_train_pred = model.predict(X_train)
 
